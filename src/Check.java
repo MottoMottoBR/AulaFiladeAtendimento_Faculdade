1b5d36107851d3301894b4130a523ff3ae691abe
MottoMottoBR <alessandro.aoc@gmail.com>
2022-10-28 14:56:13 -0400

    Initial commit

---
+++ src/Check.java
@@ -0,0 +1,12 @@
+public class FilaAtendimento {
+
+    String nome, data;
+
+    public FilaAtendimento(String goku, String s) {
+        this.data = data;
+        this.nome = nome;
+
+    }
+}
+
+
