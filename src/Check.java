468c562be7247ea3ee4ec72271dea3a5bd427923
MottoMottoBR <alessandro.aoc@gmail.com>
2022-10-28 15:00:12 -0400

    iniciando

--- src/Check.java
+++ src/Check.java
@@ -1,12 +1,57 @@
-public class FilaAtendimento {
+import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
 
-    String nome, data;
+public class Check {
 
-    public FilaAtendimento(String goku, String s) {
-        this.data = data;
-        this.nome = nome;
+    public  static void exibiMsg( String escolha, String[] vaga ) {
+        Scanner teclado = new Scanner(System.in);
+        String msgPergunta = "Digite o nome do paciente";
+
+
+        if (Objects.equals(escolha, vaga[0])) {
+            System.out.println(msgPergunta);
+            vaga[0] = escolha = teclado.nextLine();
+            Date data = new Date();
+
+        }
+        if (Objects.equals(escolha, vaga[1])) {
+            System.out.println(msgPergunta);
+            vaga[1] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[2])) {
+            System.out.println(msgPergunta);
+            vaga[2] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[3])) {
+            System.out.println(msgPergunta);
+            vaga[3] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[4])) {
+            System.out.println(msgPergunta);
+            vaga[4] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[5])) {
+            System.out.println(msgPergunta);
+            vaga[5] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[6])) {
+            System.out.println(msgPergunta);
+            vaga[6] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[7])) {
+            System.out.println(msgPergunta);
+            vaga[7] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[8])) {
+            System.out.println(msgPergunta);
+            vaga[8] = escolha = teclado.nextLine();
+        }
+        if (Objects.equals(escolha, vaga[9])) {
+            System.out.println(msgPergunta);
+            vaga[9] = escolha = teclado.nextLine();
+        }
 
     }
+
 }
-
-
--- src/Fila.java
+++ src/Fila.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Fila {
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Fila {
+
+
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        //Variavel
+        String escolha = "null";
+
+        int sim = 1;
+        int nao = 2;
+
+        String[] vaga = new String[10];
+        vaga[0] = "VAGA1";
+        vaga[1] = "VAGA2";
+        vaga[2] = "VAGA3";
+        vaga[3] = "VAGA4";
+        vaga[4] = "VAGA5";
+        vaga[5] = "VAGA6";
+        vaga[6] = "VAGA7";
+        vaga[7] = "VAGA8";
+        vaga[8] = "VAGA9";
+        vaga[9] = "VAGA10";
+
+        System.out.println("Vagas Disponiveis \n" + Arrays.toString(vaga));
+
+        System.out.print("Escolha uma vaga ");
+        escolha = ler.nextLine().toUpperCase();
+
+        //chama a funçao de esolha das vagas
+        Check.exibiMsg(escolha,vaga);
+      System.out.println( "Adicionado com sucesso! ");
+      System.out.println( "Fila atual de atendimento! ");
+      System.out.println( Arrays.toString(vaga));
+
+        System.out.println("Para adicionar ( 1 ) Remover ( 2 )  ");
+        sim = ler.nextInt();
+        if (sim == 1) {
+            System.out.println("voce escolheu adicionar! ");
+            System.out.println("Fila atual \n" + Arrays.toString(vaga));
+            System.out.print("Escolha uma vaga ");
+            escolha = ler.nextLine().toUpperCase();
+
+            //chama a funçao de esolha das vagas
+            Check.exibiMsg(escolha,vaga);
+            System.out.println("Fila atual \n" + Arrays.toString(vaga));
+        }
+
+    }
+
 }
